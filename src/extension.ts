0a258c27632017ae03f7eb347151c2ef921979a0
Rob Owens <rtowens17@gmail.com>
2022-03-15 20:51:51 -0500

    add poc code

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,15 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { PSBuildTaskProvider } from './psbuildTaskProvider';
+
+// Globals
+let psbuildTaskProvider: vscode.Disposable | undefined;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
+
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "psbuild" is now active!');
@@ -18,9 +22,21 @@ export function activate(context: vscode.ExtensionContext) {
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from PSBuild!');
 	});
-
 	context.subscriptions.push(disposable);
+
+	// Setup Task Provider
+	const workspaceRoot = (vscode.workspace.workspaceFolders && (vscode.workspace.workspaceFolders.length > 0))
+		? vscode.workspace.workspaceFolders[0].uri.fsPath : undefined;
+	if (!workspaceRoot) {
+		return;
+	}
+
+	psbuildTaskProvider = vscode.tasks.registerTaskProvider(PSBuildTaskProvider.PSBuildType, new PSBuildTaskProvider(workspaceRoot));
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+	if (psbuildTaskProvider) {
+		psbuildTaskProvider.dispose();
+	}
+}
